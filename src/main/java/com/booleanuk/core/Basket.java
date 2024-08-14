b35672b6bb26d34c12844a4bbb77cc9f9a952cb3
Iris Flystam <badde00@gmail.com>
2024-08-14 11:05:34 +0200

    GREEN: test passes

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,21 @@
 package com.booleanuk.core;
 
-public class Basket {
+import java.util.ArrayList;
 
+public class Basket {
+    ArrayList<String> bagels;
+
+    public Basket() {
+        bagels = new ArrayList<>();
+    }
+
+    public void addToBasket(String bagelType, int amount) {
+        for (int i = 0; i < amount; i++) {
+            bagels.add(bagelType);
+        }
+    }
+
+    public ArrayList<String> getBasket() {
+        return bagels;
+    }
 }
