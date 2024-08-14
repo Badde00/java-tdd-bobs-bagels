fa86a6f5b316b6be447f04c2de15607c4675c2dc
Iris Flystam <badde00@gmail.com>
2024-08-14 11:01:22 +0200

    RED: testAddingBagels added

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
-
+    @Test
+    public void testAddingBagels() {
+        Basket basket = new Basket();
+        basket.addToBasket("plain", 1);
+        Assertions.assertEquals(1, basket.getBasket().size());
+    }
 }
